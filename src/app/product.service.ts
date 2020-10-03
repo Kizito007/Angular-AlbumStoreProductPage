d69da0a1a74f90e90ef59053a22def8867f034df
Kizito007 <nwakakelechi49@gmail.com>
2020-10-03 19:46:46 +0100

    using interface to describe album

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Http , Response } from '@angular/http';
 import 'rxjs/add/operator/map';
-//import {Album} from './album';<Album>
+import {Album} from './album';
+import {Observable} from 'rxjs/Observable';
+//<Album>
 
 @Injectable()
 export class ProductService {
 
   constructor(private _http: Http) { }
   private _albumUrl = '../assets/album.json'
-  getAlbum(id:number){
+  getAlbum(id:number):Observable<Album>{
     return this._http.get(this._albumUrl)
-    .map((response) => response.json())
+    .map((response) => <Album>response.json())
   }
+
 }
